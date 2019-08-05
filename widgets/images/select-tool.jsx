f02a908b8dab296d31597d07f4aebd45baff2f37
hectorhon <hon.lianhung@gmail.com>
2019-08-06 07:08:21 +0800

    Draw small rectangles using div

--- widgets/images/select-tool.jsx
+++ widgets/images/select-tool.jsx
@@ -16,15 +16,15 @@ class SelectTool extends React.Component {
            onMouseMoveCapture={e => this.handleMouseMove(e)}
            onMouseUpCapture={e => this.handleMouseUp(e)} >
           {this.state.selection &&
-           <Rectangle x1={this.state.selection.x1}
-                      y1={this.state.selection.y1}
-                      x2={this.state.selection.x2}
-                      y2={this.state.selection.y2} />}
+           <SelectRegion x1={this.state.selection.x1}
+                         y1={this.state.selection.y1}
+                         x2={this.state.selection.x2}
+                         y2={this.state.selection.y2} />}
           {this.state.point1 && this.state.point2 &&
-           <Rectangle x1={this.state.point1.x}
-                      y1={this.state.point1.y}
-                      x2={this.state.point2.x}
-                      y2={this.state.point2.y} />}
+           <SelectRegion x1={this.state.point1.x}
+                         y1={this.state.point1.y}
+                         x2={this.state.point2.x}
+                         y2={this.state.point2.y} />}
           <img className={this.props.imgClasses}
                src={this.props.imageSrc}
                onClick={() => this.props.callback()}
@@ -52,16 +52,28 @@ class SelectTool extends React.Component {
 
   handleMouseUp(e) {
     e.preventDefault();
-    this.setState({
-      point1: null,
-      point2: null,
-      selection: {
-        x1: this.state.point1.x,
-        y1: this.state.point1.y,
-        x2: this.state.point2.x,
-        y2: this.state.point2.y
-      }
-    });
+    const MIN_WIDTH = 100;
+    const MIN_HEIGHT = 100;
+    const width = Math.abs(this.state.point1.x - this.state.point2.x);
+    const height = Math.abs(this.state.point1.y - this.state.point2.y);
+    if (width > MIN_WIDTH && height > MIN_HEIGHT) {
+      this.setState({
+        point1: null,
+        point2: null,
+        selection: {
+          x1: this.state.point1.x,
+          y1: this.state.point1.y,
+          x2: this.state.point2.x,
+          y2: this.state.point2.y
+        }
+      });
+    } else {
+      this.setState({
+        point1: null,
+        point2: null,
+        selection: null
+      });
+    }
   }
 
   getRelativeMousePosition(e) {
@@ -73,22 +85,83 @@ class SelectTool extends React.Component {
 
 }
 
+class SelectRegion extends React.Component {
+
+  render() {
+    const x1 = Math.min(this.props.x1, this.props.x2);
+    const y1 = Math.min(this.props.y1, this.props.y2);
+    const x2 = Math.max(this.props.x1, this.props.x2);
+    const y2 = Math.max(this.props.y1, this.props.y2);
+    const width = x2 - x1;
+    const height = y2 - y1;
+
+    const rows = 40;
+    const columns = 5;
+    const spacing = 4;
+    const boxWidth = Math.floor((width - spacing) / columns - spacing);
+    const boxHeight = Math.floor((height - spacing) / rows - spacing);
+    console.log(boxWidth, boxHeight);
+    var boxes = [];
+    if (boxWidth > spacing + 4 && boxHeight > spacing + 4) {
+      for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < columns; col++) {
+          boxes.push({
+            x: spacing + col * (boxWidth + spacing),
+            y: spacing + row * (boxHeight + spacing),
+          });
+        }
+      }
+    }
+    return (
+      <Rectangle x={x1} y={y1} width={width} height={height}>
+          {boxes.map(({ x, y }, index) => (
+            <Rectangle x={x} y={y} width={boxWidth} height={boxHeight}
+                       key={index} />
+          ))}
+      </Rectangle>
+    );
+  }
+
+}
+
 class Rectangle extends React.Component {
 
   render() {
-    const _x1 = Math.min(this.props.x1, this.props.x2);
-    const _y1 = Math.min(this.props.y1, this.props.y2);
-    const _x2 = Math.max(this.props.x1, this.props.x2);
-    const _y2 = Math.max(this.props.y1, this.props.y2);
     return (
       <div style={{
         position: "absolute",
-        left: _x1 + "px",
-        top: _y1 + "px",
-        width: (_x2 - _x1) + "px",
-        height: (_y2 - _y1) + "px",
+        left: this.props.x + "px",
+        top: this.props.y + "px",
+        width: this.props.width + "px",
+        height: this.props.height + "px",
         border: "1px solid black",
-      }}></div>
+      }}>
+          {this.props.children}
+      </div>
+    );
+  }
+
+}
+
+class Rectangle0 extends React.Component {
+
+  render() {
+    const x1 = Math.min(this.props.x1, this.props.x2);
+    const y1 = Math.min(this.props.y1, this.props.y2);
+    const x2 = Math.max(this.props.x1, this.props.x2);
+    const y2 = Math.max(this.props.y1, this.props.y2);
+    const width = x2 - x1;
+    const height = y2 - y1;
+    return (
+      <div style={{
+        position: "absolute",
+        left: x1 + "px",
+        top: y1 + "px",
+        width: width + "px",
+        height: height + "px",
+        border: "1px solid black",
+      }}>
+      </div>
     );
   }
 
