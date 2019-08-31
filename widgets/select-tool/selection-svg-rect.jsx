c3d6dcb465d794dccc91b82cb3d339d3d44021f4
hectorhon <hon.lianhung@gmail.com>
2019-08-31 23:12:53 +0800

    Little rectangles

--- widgets/select-tool/selection-svg-rect.jsx
+++ widgets/select-tool/selection-svg-rect.jsx
@@ -6,15 +6,43 @@ import Selection from './selection'
 function SelectionSvgRect(props) {
   const { selection } = props
   const {
-    x, y, width, height
+    x: selectionX, y: selectionY, width: selectionWidth, height: selectionHeight
   } = selection
+  const rows = 20
+  const columns = 4
+  const spacingX = 4
+  const spacingY = 4
+  const gridWidth = (selectionWidth - (columns - 1) * spacingX) / columns
+  const gridHeight = (selectionHeight - (rows - 1) * spacingY) / rows
+  const grids = []
+  for (let i = 0; i < rows; i += 1) {
+    for (let j = 0; j < columns; j += 1) {
+      grids.push({
+        x: selectionX + j * (gridWidth + spacingX),
+        y: selectionY + i * (gridHeight + spacingY),
+        width: gridWidth,
+        height: gridHeight
+      })
+    }
+  }
   return (
-    <rect x={x}
-          y={y}
-          width={width}
-          height={height}
-          stroke="black"
-          fill="none" />
+    <g>
+      <rect x={selectionX}
+            y={selectionY}
+            width={selectionWidth}
+            height={selectionHeight}
+            stroke="black"
+            fill="none" />
+      {grids.map((grid, index) => (
+        <rect key={index}
+              x={grid.x}
+              y={grid.y}
+              width={grid.width}
+              height={grid.height}
+              stroke="black"
+              fill="none" />
+      ))}
+    </g>
   )
 }
 
