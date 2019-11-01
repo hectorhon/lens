01cad86b370b4c6355cd7233e0f892e81c8a5463
hectorhon <hon.lianhung@gmail.com>
2019-11-01 23:44:28 +0800

    react-redux

--- widgets/table/index.jsx
+++ widgets/table/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import Table from './table'
 
@@ -11,7 +13,21 @@ const data = [{
   age: 34
 }]
 
+const store = createStore((state, action) => {
+  if (typeof state === 'undefined') {
+    const initialState = {
+      data
+    }
+    return initialState
+  }
+  return state
+})
+
 ReactDOM.render(
-  <Table data={data} columnNames={['name', 'age']} />,
+  (
+    <Provider store={store}>
+      <Table data={data} columnNames={['name', 'age']} />
+    </Provider>
+  ),
   document.getElementById('root')
 )
