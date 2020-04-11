0f6ef550861e49e5b91965f1c668e11426e5875a
hectorhon <hon.lianhung@gmail.com>
2020-04-11 12:40:35 +0800

    ejs

--- server/index.js
+++ server/index.js
@@ -4,8 +4,17 @@ const contacts = require('./contacts/routes')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs')
+app.set('views', '.')
+
 app.use('/', contacts)
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  res.render('index', {
+    number: new Date()
+  })
+})
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
